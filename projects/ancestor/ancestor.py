55d3d61a8d5e1a9731d2e3e0dac8379cad37ffea
ScottLightfoot <gslightfoot@gmail.com>
2020-09-09 15:56:31 -0500

    recursion is just a way to tell people they should think you are clever.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,39 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # graph of each child's parents
+    cgr = {}
+
+    for p, c in ancestors:
+        if c in cgr:
+            cgr[c].append(p)
+        else:
+            cgr[c] = [p]
+
+    longest = []
+    length = 1
+    lines = [[starting_node]]
+    while lines:
+        curr = lines.pop(0)
+        if len(curr) == length:
+            longest.append(curr)
+        elif len(curr) > length:
+            longest = [curr]
+            length = len(curr)
+        c = curr[-1]
+        if c in cgr:
+            for i in cgr[c]:
+                nxt = curr.copy()
+                nxt.append(i)
+                lines.append(nxt)
+    if len(longest) == 1:
+        if longest[0][-1] == starting_node:
+            return -1
+        else:
+            return longest[0][-1]
+    derp = [i[-1] for i in longest]
+    return min(derp)
+
+
+
+            
+
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,7 +1,29 @@
+# from graph import Graph
+# gr = Graph()
+# gr.add_vertex(1)
+# gr.add_vertex(2)
+# gr.add_vertex(3)
+# gr.add_vertex(4)
+# gr.add_vertex(5)
+# gr.add_vertex(6)
+# gr.add_vertex(7)
+# gr.add_edge(5, 3)
+# gr.add_edge(6, 3)
+# gr.add_edge(7, 1)
+# gr.add_edge(4, 7)
+# gr.add_edge(1, 2)
+# gr.add_edge(7, 6)
+# gr.add_edge(2, 4)
+# gr.add_edge(3, 5)
+# gr.add_edge(2, 3)
+# gr.add_edge(4, 6)
+
 """
 Simple graph implementation
 """
+from pkg_resources import DEVELOP_DIST
 from util import Stack, Queue  # These may come in handy
+import sys, io
 
 class Graph:
 
@@ -9,72 +31,189 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
+
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
+
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v2 not in self.vertices:
+            raise Exception(f'verticy "{v2}" not present in graph')
+        else:
+            self.vertices[v1].add(v2)
+
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
-    def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = []
+        stack = [starting_vertex]
+        while stack:
+            curr = stack.pop()
+            visited.append(curr)
+            nxt = [i for i in self.vertices[curr] if i not in visited and i not in stack]
+            stack.extend(nxt)
+            # breakpoint(dri)
 
-    def dft_recursive(self, starting_vertex):
+        for i in visited:
+            print(i)
+
+
+    def dft_recursive(self, starting_vertex, visited = None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited == None:
+            print(starting_vertex)
+            visited = [starting_vertex]
+        new_nodes = [i for i in self.vertices[starting_vertex] if i not in visited]
+        for i in new_nodes:
+            if i not in visited:
+                print(i)
+                visited.append(i)
+                self.dft_recursive(i, visited)
 
-    def bfs(self, starting_vertex, destination_vertex):
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        visited = []
+        path = [starting_vertex]
+        while path:
+            curr = path.pop(0)
+            nxt = [i for i in self.vertices[curr] if i not in visited]
+            visited.append(curr)
+            path.extend(nxt)
+        for i in visited:
+            print(i)
+
+
+    def bfs(self, sv, dv):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        visited = [False] * (len(self.vertices) + 1)
+        paths = [[sv]]
+        visited[sv] = True
+        while paths:
+            curr = paths.pop(0)
+            visited[curr[-1]] = True
+            if dv in self.vertices[curr[-1]]:
+                curr.append(dv)
+                return curr
+            nvs = [j for j in list(self.vertices[curr[-1]]) if j not in visited]
+            for k in nvs:
+                nxt = curr.copy()
+                nxt.append(k)
+                paths.append(nxt)
+        return False
 
-    def dfs(self, starting_vertex, destination_vertex):
+    def dfs(self, sv, dv):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        visited = [False] * (len(self.vertices) + 1)
+        paths = [[sv]]
+        visited[sv] = True
+        while paths:
+            curr = paths.pop()
+            visited[curr[-1]] = True
+            if dv in self.vertices[curr[-1]]:
+                curr.append(dv)
+                return curr
+            nvs = [j for j in list(self.vertices[curr[-1]]) if j not in visited]
+            for k in nvs:
+                nxt = curr.copy()
+                nxt.append(k)
+                paths.append(nxt)
+        return False
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
+            """
+            Return a list containing a path from
+            starting_vertex to destination_vertex in
+            depth-first order.
+            This should be done using recursion.
+            """
+            # if our visited set does not exist, create it
+            if visited is None:
+                visited = set()
+            # if our path does not exist, create it
+            if path is None:
+                path = []
+            # mark our starting vertex as visited
+            visited.add(starting_vertex)
+            # add our starting vertex to our path
+            path = path + [starting_vertex]
+            # if the starting vertex is our destination vertex, return its path
+            if starting_vertex == destination_vertex:
+                return path
+            # for each neighbor of our starting vertex,
+            for neighbor in self.get_neighbors(starting_vertex):
+                # if the neighbor hasn't been visited
+                if neighbor not in visited:
+                    # create a new path and recursively call dfs_recursive on it
+                    new_path = self.dfs_recursive(
+                        neighbor, destination_vertex, visited, path)
+                    # if the new path exists, return it
+                    if new_path:
+                        return new_path
+            # we did not find the destination vertex
+            return None
 
-        This should be done using recursion.
-        """
-        pass  # TODO
+    # def dfs_recursive(self, sv, dv, visited = None, path = None):
+    #     """
+    #     Return a list containing a path from
+    #     starting_vertex to destination_vertex in
+    #     depth-first order.
+
+    #     This should be done using recursion.
+    #     """
+    #     # first settings for init
+    #     if visited == None:
+    #         visited = [False] * (len(self.vertices) + 1)
+    #     if path == None:
+    #         path = []
+
+    #     path.append(sv)
+
+    #     visited[sv] = True
+
+    #     if dv in self.vertices[sv]:
+    #         return path.append(dv)
+
+    #     for i in self.vertices[sv]:
+    #         if not visited[i]:
+    #             breakpoint()
+    #             out = self.dfs_recursive(i, dv, visited, path)
+    #             if out is not None:
+    #                 return out
+    #     return None
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
